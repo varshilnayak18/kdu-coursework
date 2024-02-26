8d69ca837dba6b82085100709732fcc92c872b0e
varshilnayak18 <varshilshaileshbhai.nayak@kickdrumtech.com>
2024-02-26 15:05:10 +0530

    <fe-homework-14 : added useref homework>

---
+++ frontend/homework/14/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Form from './components/Form/Form'
+import ScrollToTop from './components/ScrollToTop/ScrollToTop'
+import Timer from './components/Timer/Timer'
+
+function App() {
+
+  return (
+    <>
+      <Form/>
+      <Timer/>
+      <ScrollToTop/>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/homework/14/src/components/ScrollToTop/ScrollToTop.styles.ts
@@ -0,0 +1,10 @@
+export const scrollToTop: React.CSSProperties = {
+    height: "400px", 
+    overflow: "auto",
+    backgroundColor: 'lightblue',
+    textAlign: 'center'
+} 
+
+export const para: React.CSSProperties = {
+    textAlign: 'center'
+}
